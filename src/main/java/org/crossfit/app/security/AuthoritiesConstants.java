9290d6147956243cf4743f60ae822a3784aca4f2
lgangloff <loic.gangloff@gmail.com>
2015-09-01 21:51:47 +0200

    Début gestion des membres

--- src/main/java/org/crossfit/app/security/AuthoritiesConstants.java
+++ src/main/java/org/crossfit/app/security/AuthoritiesConstants.java
@@ -11,8 +11,6 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
     
     public static final String MANAGER = "ROLE_MANAGER";
-
-    public static final String MANAGER = "ROLE_MANAGER";
     
     public static final String USER = "ROLE_USER";
 

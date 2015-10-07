859fa910435ffba57be645ae53e744772d529cbd
gcolin <guillaume.colin@pharmagest.com>
2015-10-07 17:36:17 +0200

    FIX : Affichage de créneaux réservés alors qu'il ne le sont pas

--- src/main/java/org/crossfit/app/repository/BookingRepository.java
+++ src/main/java/org/crossfit/app/repository/BookingRepository.java
@@ -17,7 +17,7 @@ public interface BookingRepository extends JpaRepository<Booking,Long> {
     @Query("select b from Booking b where b.box =:box AND b.startAt between :start and :end")
 	List<Booking> findAll(@Param("box") CrossFitBox box, @Param("start") DateTime start, @Param("end") DateTime end);
     
-    @Query("select b from Booking b where b.box =:box AND b.owner = :member AND b.startAt between :start and :end")
+    @Query("select b from Booking b where b.box =:box AND b.owner = :member AND b.startAt = :start and b.endAt = :end")
 	List<Booking> findAllByMember(@Param("box") CrossFitBox box, @Param("member") Member member, @Param("start") DateTime start, @Param("end") DateTime end);
 
 }

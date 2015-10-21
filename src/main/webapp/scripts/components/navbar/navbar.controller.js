6b658d5202959ca117b38cf13609fe6ef6f4b2fe
gcolin <guillaume.colin@pharmagest.com>
2015-10-21 14:41:46 +0200

    STYLE : mise en place d'un nouveau style

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -2,11 +2,17 @@
 
 angular.module('crossfitApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV) {
-        $scope.isAuthenticated = Principal.isAuthenticated;
+    	
+    	Principal.identity().then(function(account) {
+            $scope.account = account;
+            $scope.isAuthenticated = Principal.isAuthenticated;
+        });
+    	
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
 
         $scope.logout = function () {
+        	$scope.account = null;
             Auth.logout();
             $state.go('home');
         };

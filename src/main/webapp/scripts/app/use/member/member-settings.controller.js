a27f92a289106f32bd725bea1a6f1f3169e64d01
gcolin <guillaume.colin@pharmagest.com>
2015-11-04 11:24:55 +0100

    FIX : La date de fin d’adhésion est jj/mm/aaaa quand elle est vide + interface profil membre

--- src/main/webapp/scripts/app/use/member/member-settings.controller.js
+++ src/main/webapp/scripts/app/use/member/member-settings.controller.js
@@ -4,25 +4,13 @@ angular.module('crossfitApp')
     .controller('MemberSettingsController', 
     		['$scope', '$filter',  '$stateParams', '$state', 'CurrentMember', 'Member', 'User',
     	        function($scope, $filter, $stateParams, $state, CurrentMember, Member, User) {
-		$scope.success = null;
-        $scope.error = null;
-    	$scope.member = CurrentMember.get();
+		
     	
-    	$scope.member.$promise.then(function(member) {
-    		member.level = $filter('translate')('crossfitApp.Level.'+member.level);
+    	CurrentMember.get().$promise.then(function(member) {
+    		$scope.member = member;
     	});
 
-    	console.log($scope.member);
-
-
     	$scope.save = function () {
-    		Member.update($scope.member).then(function() {
-                $scope.error = null;
-                $scope.success = 'OK';
-                
-            }).catch(function() {
-                $scope.success = null;
-                $scope.error = 'ERROR';
-            });
+    		Member.update($scope.member);
         };
     }]);
